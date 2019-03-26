8bfcca67481aad362e034c05ffd2a2cb6e8df20a
doanHa <dmha@uh.edu>
2019-03-26 09:53:23 -0500

    Factor Code Structure 2

--- src/main/java/com/java/Bank.java
+++ src/main/java/com/java/Bank.java
@@ -11,18 +11,6 @@ public class Bank {
 	}
 
 
-	private static void employeeLogin() {
-		System.out.println("Welcome to work! Please log in");
-		System.out.println("Please enter your username: ");
-		String usernameInput = getInput();
-		System.out.println("Please enter your password: ");
-		String passwordInput = getInput();
-		// TODO use usernameInput and passwordInput to check for validation
-		// TODO then show them the menu of what they can do
-
-	}
-
-
 
 	/*
 	 * static Logger logger= Logger.getLogger(Demo1.class);
