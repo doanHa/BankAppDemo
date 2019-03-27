86b8be2fdf857cecdf1f003eb166f5286dccd833
doanHa <dmha@uh.edu>
2019-03-26 22:36:14 -0500

    Minor changed for CustomerUI.java and Bank.java

--- src/main/java/com/java/Bank.java
+++ src/main/java/com/java/Bank.java
@@ -7,11 +7,7 @@ public class Bank {
 	// values for records that do not exist
 	// (i.e user passing in wrong log in info, same with employer)
 	public static void main(String[] args) {
-		AccountDaoImpl act = new AccountDaoImpl();
-		Customer custom = new Customer();
-		custom.setCustomerID(2005);
-		act.insertBankAccount(custom, 11248, 'S', 'Y',1);
-		//UserUI.showMainMenu();
+		UserUI.showMainMenu();
 	}
 	/*
 	 * static Logger logger= Logger.getLogger(Demo1.class);
